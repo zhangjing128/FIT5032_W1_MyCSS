034a3cf80ee57b9541b89f1b23c2c689ab6551a5
ZJ <542646790@qq.com>
2022-07-02 17:16:54 +0800

    添加项目文件。

---
+++ lab1_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab1_2.Startup))]
+namespace lab1_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
